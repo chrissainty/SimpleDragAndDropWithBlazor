40d464350b5eb587b4f2fd3f7ac646e30e921f3a
Chris Sainty <chris@chrissainty.com>
2019-08-09 21:53:35 +0100

    Initial commit

---
+++ SimpleDragAndDropWithBlazor/SimpleDragAndDropWithBlazor/Shared/Task.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SimpleDragAndDropWithBlazor.Shared
+{
+    public class Task
+    {
+        public int Id { get; set; }
+        public TaskStatuses Status { get; set; }
+        public string Description { get; set; }
+        public DateTime LastUpdated { get; set; }
+    }
+
+    public enum TaskStatuses
+    {
+        Todo,
+        Started,
+        Completed
+    }
+}
