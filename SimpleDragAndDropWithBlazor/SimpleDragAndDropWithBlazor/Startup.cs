07710c5ea14456ddcc7e21eca19d7e207e38a9ff
Chris Sainty <chris@chrissainty.com>
2019-09-19 10:51:08 +0100

    Updated to RC1

--- SimpleDragAndDropWithBlazor/SimpleDragAndDropWithBlazor/Startup.cs
+++ SimpleDragAndDropWithBlazor/SimpleDragAndDropWithBlazor/Startup.cs
@@ -20,7 +20,9 @@ namespace SimpleDragAndDropWithBlazor
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
-            services.AddServerSideBlazor();
+            services.AddServerSideBlazor(config => {
+                config.DetailedErrors = true;
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
